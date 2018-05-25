10e269a42f4cb5e218b1596d2d18e70dafbe55a5
Tony <yyy333567@163.com>
2018-05-25 18:37:21 +0800

    commit

---
+++ app/src/main/java/com/huatec/myapplication/MainActivity.java
@@ -0,0 +1,84 @@
+package com.huatec.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+
+import cn.jpush.android.api.InstrumentedActivity;
+
+
+/**
+ * 程序入口
+ */
+public class MainActivity extends InstrumentedActivity {
+
+    public static boolean isForeground = false;
+    private static final String TAG = "MainActivity";
+    private MessageReceiver mMessageReceiver;
+    public static final String MESSAGE_RECEIVED_ACTION = "com.example.jpushdemo.MESSAGE_RECEIVED_ACTION";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_EXTRAS = "extras";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerMessageReceiver();
+    }
+
+    /**
+     * 注册本地广播
+     */
+    public void registerMessageReceiver() {
+        mMessageReceiver = new MessageReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+        filter.addAction(MESSAGE_RECEIVED_ACTION);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, filter);
+    }
+
+    public class MessageReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            try {
+                if (MESSAGE_RECEIVED_ACTION.equals(intent.getAction())) {
+                    String messge = intent.getStringExtra(KEY_MESSAGE);
+                    String extras = intent.getStringExtra(KEY_EXTRAS);
+                    StringBuilder showMsg = new StringBuilder();
+                    showMsg.append(KEY_MESSAGE + " : " + messge + "\n");
+                    if (!ExampleUtil.isEmpty(extras)) {
+                        showMsg.append(KEY_EXTRAS + " : " + extras + "\n");
+                    }
+                    //自定义消息为：showMsg.toString()
+                    Log.d(TAG, "onReceive: "+showMsg.toString());
+                }
+            } catch (Exception e){
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        isForeground = true;
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        isForeground = false;
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/huatec/myapplication/TestActivity.java
@@ -0,0 +1,36 @@
+package com.huatec.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TextView;
+
+import cn.jpush.android.api.JPushInterface;
+
+/**
+ * 点击通知跳转到此Activity
+ */
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("用户自定义打开的Activity");
+
+        Intent intent = getIntent();
+        if (null != intent) {
+	        Bundle bundle = getIntent().getExtras();
+            String title = null;
+            String content = null;
+            if(bundle!=null){
+                title = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_TITLE);
+                content = bundle.getString(JPushInterface.EXTRA_ALERT);
+            }
+            tv.setText("Title : " + title + "  " + "Content : " + content);
+        }
+        addContentView(tv, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+    }
+
+}
